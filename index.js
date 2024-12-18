503bf1985c5582cdbeda149043caf7e361b16f26
DanielAlabarze <139582202+DanielAlabarze@users.noreply.github.com>
2024-12-18 10:02:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const bsCollapse = new bootstrap.Collapse("#navbarNav", {
+  toggle: false,
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".navbar a span")) return false;
+
+  bsCollapse.hide();
+});
+
+
